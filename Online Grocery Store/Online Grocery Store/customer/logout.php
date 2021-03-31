190124830e35e54073635a2c3be20a32bdb8f91c
kunju004 <kunjpatel3103@gmail.com>
2021-03-31 19:29:37 +0530

    Online Grocery Store

--- Online Grocery Store/Online Grocery Store/customer/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['USER_LOGIN']);
-unset($_SESSION['USER_EMAIL']);
-unset($_SESSION['USER_REGISTER']);
-unset($_SESSION['MESSAGE']);
-header('location:login.php');
-die();
-?>
